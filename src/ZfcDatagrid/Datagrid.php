5a7d67d96020f8bb947170c7f25bc959fd796419
Martin Keckeis <martin.keckeis1@gmail.com>
2013-04-29 09:36:50 +0300

    Create Datagrid.php

---
+++ src/ZfcDatagrid/Datagrid.php
@@ -0,0 +1,18 @@
+<?php
+namespace ZfcDatagrid;
+
+use ZfcDatagrid\DataSource;
+use ZfcDatagrid\Renderer;
+
+use Zend\Http\PhpEnvironment\Request;
+use Zend\Stdlib\ResponseInterface;
+use Zend\View\Model\ViewModel;
+
+use Zend\Paginator\Paginator;
+use Zend\I18n\Translator\Translator;
+use Doctrine\ORM\QueryBuilder;
+use ArrayIterator;
+
+class Datagrid
+{
+}
