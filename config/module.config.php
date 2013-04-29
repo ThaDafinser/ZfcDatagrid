4690ef0af34d2076f9786102d1c0e262994e2d84
Martin Keckeis <martin.keckeis1@gmail.com>
2013-04-29 09:36:05 +0300

    Create module.config.php

---
+++ config/module.config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'view_manager' => array(
+        'template_map' => array(
+            'bootstrap/table' => __DIR__ . '/../view/bootstrap/table.phtml'
+        ),
+        'template_path_stack' => array(
+            'ZfcDatagrid' => __DIR__ . '/../view'
+        )
+    )
+);
