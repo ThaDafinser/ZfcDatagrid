475c85036e5bc8c4762619b9aeebce7e82fe65b4
Martin Keckeis <martin.keckeis1@gmail.com>
2013-04-30 13:39:30 +0300

    Update ExampleController.php

--- src/ZfcDatagrid/Controller/ExampleController.php
+++ src/ZfcDatagrid/Controller/ExampleController.php
@@ -11,10 +11,103 @@ class ExampleController extends AbstractActionController
 {
 
     /**
+     * Simple bootstrap table
+     *
+     * @return \ZfcDatagrid\Controller\ViewModel
+     */
+    public function bootstrapAction ()
+    {
+        /* @var $dataGrid \ZfcDatagrid\Datagrid */
+        $dataGrid = $this->getServiceLocator()->get('zfcDatagrid');
+        $dataGrid->setTitle('Persons');
+        $dataGrid->setRowsPerPage(5);
+        $dataGrid->setDataSource($this->getDataArray());
+        
+        $col = new Column\Standard('id');
+        $col->setIdentity();
+        $dataGrid->addColumn($col);
+        
+        $col = new Column\Standard('displayName');
+        $col->setLabel('Displayname');
+        $col->setWidth(25);
+        $col->setSortDefault(1, 'ASC');
+        $col->addStyle(new Style\Bold());
+        $dataGrid->addColumn($col);
+        
+        $col = new Column\Standard('familyName');
+        $col->setLabel('Familyname');
+        $col->setWidth(15);
+        $dataGrid->addColumn($col);
+        
+        $col = new Column\Standard('givenName');
+        $col->setLabel('Givenname');
+        $col->setWidth(15);
+        $col->setSortDefault(2, 'DESC');
+        $dataGrid->addColumn($col);
+        
+        $col = new Column\Standard('gender');
+        $col->setLabel('Gender');
+        $col->setWidth(10);
+        $col->setReplaceValues(array(
+            'm' => 'male',
+            'f' => 'female'
+        ));
+        $col->setTranslationEnabled(true);
+        $dataGrid->addColumn($col);
+        
+        {
+            $col = new Column\Standard('age');
+            $col->setLabel('Age');
+            $col->setWidth(5);
+            $col->setType(new Type\Number());
+            
+            $style = new Style\Color\Red();
+            $style->setByValue($col, 20);
+            $col->addStyle($style);
+            
+            $dataGrid->addColumn($col);
+        }
+        
+        {
+            $colType = new Type\Number();
+            $colType->addAttribute(\NumberFormatter::FRACTION_DIGITS, 2);
+            $colType->setSuffix(' kg');
+            
+            $col = new Column\Standard('weight');
+            $col->setLabel('Weight');
+            $col->setWidth(10);
+            $col->setType($colType);
+            $dataGrid->addColumn($col);
+        }
+        
+        $col = new Column\Standard('birthday');
+        $col->setLabel('Birthday');
+        $col->setWidth(10);
+        $col->setType(new Type\Date());
+        $dataGrid->addColumn($col);
+        
+        {
+            $colType = new Type\Date('H:i:s d.m.y', \IntlDateFormatter::MEDIUM, \IntlDateFormatter::MEDIUM);
+            $colType->setSourceTimezone('Europe/Vienna');
+            $colType->setOutputTimezone('UTC');
+            
+            $col = new Column\Standard('changeDate');
+            $col->setLabel('Last change');
+            $col->setWidth(15);
+            $col->setType($colType);
+            $dataGrid->addColumn($col);
+        }
+        
+        $dataGrid->execute();
+        
+        return $dataGrid->getResponse();
+    }
+
+    /**
      * Usage
      * php index.php show example grid --page 1
      * php index.php show example grid --page 2
-     * 
+     *
      * @return \Zend\Http\Response\Stream
      */
     public function consoleAction ()
@@ -23,72 +116,7 @@ class ExampleController extends AbstractActionController
         $dataGrid = $this->getServiceLocator()->get('zfcDatagrid');
         $dataGrid->setTitle('Persons');
         $dataGrid->setRowsPerPage(5);
-        
-        $row = array(
-            'id' => 1,
-            'displayName' => 'Very loooooong name..........',
-            'familyName' => 'Family',
-            'givenName' => '123',
-            'age' => 35,
-            'weight' => 50,
-            'birthday' => '1987-10-03',
-            'changeDate' => '14:30:41 19.04.01'
-        );
-        $row2 = array(
-            'id' => 2,
-            'displayName' => 'Aaa',
-            'givenName' => 'Bbb',
-            'age' => 20,
-            'weight' => 123.12222,
-            'birthday' => '1981-01-31',
-            'changeDate' => '22:30:41 31.12.99'
-        );
-        $row3 = array(
-            'id' => 3,
-            'displayName' => 'Aaa',
-            'givenName' => 'Aaaa',
-            'age' => 23,
-            'weight' => 70.23,
-            'birthday' => '1991-10-03',
-            'changeDate' => '09:30:41 19.04.13'
-        );
-        $row4 = array(
-            'id' => 5,
-            'displayName' => 'Zzz',
-            'familyName' => 'Family',
-            'givenName' => '123',
-            'age' => 35,
-            'weight' => 50,
-            'birthday' => '1987-10-03',
-            'changeDate' => '14:30:41 19.04.01'
-        );
-        $row5 = array(
-            'id' => 5,
-            'displayName' => 'Aaa',
-            'givenName' => 'Ccc',
-            'age' => 20,
-            'weight' => 123.12222,
-            'birthday' => '1981-01-31',
-            'changeDate' => '22:30:41 31.12.99'
-        );
-        $row6 = array(
-            'id' => 6,
-            'displayName' => 'Aaa',
-            'givenName' => 'Aaaa',
-            'age' => 23,
-            'weight' => 70.23,
-            'birthday' => '1991-10-03',
-            'changeDate' => '09:30:41 19.04.13'
-        );
-        $data = array(
-            $row,
-            $row2,
-            $row3,
-            $row4,
-            $row5,
-            $row6
-        );
-        $dataGrid->setDataSource($data);
+        $dataGrid->setDataSource($this->getDataArray());
         
         $col = new Column\Standard('id');
         $col->setIdentity();
@@ -120,5 +148,82 @@ class ExampleController extends AbstractActionController
         $dataGrid->execute();
         
         return $dataGrid->getResponse();
+    }
+
+    private function getDataArray ()
+    {
+        $row = array(
+            'id' => 1,
+            'displayName' => 'Very loooooong name..........',
+            'familyName' => 'Family',
+            'givenName' => '123',
+            'gender' => 'f',
+            'age' => 35,
+            'weight' => 50,
+            'birthday' => '1987-10-03',
+            'changeDate' => '14:30:41 19.04.01'
+        );
+        $row2 = array(
+            'id' => 2,
+            'displayName' => 'Aaa',
+            'givenName' => 'Bbb',
+            'gender' => 'm',
+            'age' => 20,
+            'weight' => 123.12222,
+            'birthday' => '1981-01-31',
+            'changeDate' => '22:30:41 31.12.99'
+        );
+        $row3 = array(
+            'id' => 3,
+            'displayName' => 'Aaa',
+            'givenName' => 'Aaaa',
+            'gender' => 'm',
+            'age' => 23,
+            'weight' => 70.23,
+            'birthday' => '1991-10-03',
+            'changeDate' => '09:30:41 19.04.13'
+        );
+        $row4 = array(
+            'id' => 5,
+            'displayName' => 'Zzz',
+            'familyName' => 'Family',
+            'givenName' => '123',
+            'gender' => 'f',
+            'age' => 35,
+            'weight' => 50,
+            'birthday' => '1987-10-03',
+            'changeDate' => '14:30:41 19.04.01'
+        );
+        $row5 = array(
+            'id' => 5,
+            'displayName' => 'Aaa',
+            'givenName' => 'Ccc',
+            'gender' => 'm',
+            'age' => 20,
+            'weight' => 123.12222,
+            'birthday' => '1981-01-31',
+            'changeDate' => '22:30:41 31.12.99'
+        );
+        $row6 = array(
+            'id' => 6,
+            'displayName' => 'Aaa',
+            'givenName' => 'Aaaa',
+            'gender' => 'f',
+            'age' => 23,
+            'weight' => 70.23,
+            'birthday' => '1991-10-03',
+            'changeDate' => '09:30:41 19.04.13'
+        );
+        
+        $data = array(
+            $row,
+            $row2,
+            $row3,
+            $row4,
+            $row5,
+            $row6
+        );
+        
+        return $data;
     }
 }
