eaf91d949281974b0fa12c2b5bee4054a93c2451
Martin Keckeis <martin.keckeis1@gmail.com>
2013-04-30 13:37:35 +0300

    Create Standard.php

---
+++ src/ZfcDatagrid/Column/Standard.php
@@ -0,0 +1,41 @@
+<?php
+namespace ZfcDatagrid\Column;
+
+class Standard extends AbstractColumn
+{
+
+    /**
+     * Possible calls:
+     * $column = new Column('id', 'user')
+     * Select the id from the user table -> UNIQUE is the comination of both
+     * $column = new Column('title')
+     * Just the title from an array -> UNIQUE will be just the first parameter
+     * $column = new Column('(SELECT GROUP_CONCAT....)', 'someAlias')
+     * Use the subselect -> UNIQUE will be the second parameter
+     *
+     * @param string|object $columnOrIndexOrObject            
+     * @param string $tableOrAliasOrUniqueId            
+     */
+    public function __construct ($columnOrIndexOrObject, $tableOrAliasOrUniqueId = null)
+    {
+        if ($tableOrAliasOrUniqueId !== null && ! is_string($tableOrAliasOrUniqueId)) {
+            throw new \Exception('Variable $tableOrAliasOrUniqueId must be null or a string');
+        }
+        
+        if (is_string($columnOrIndexOrObject) && $tableOrAliasOrUniqueId !== null) {
+            // $column = new Column('id', 'user')
+            $this->setUniqueId($tableOrAliasOrUniqueId . '_' . $columnOrIndexOrObject);
+            $this->setSelect($tableOrAliasOrUniqueId, $columnOrIndexOrObject);
+        } elseif (is_string($columnOrIndexOrObject)) {
+            // $column = new Column('title')
+            $this->setUniqueId($columnOrIndexOrObject);
+            $this->setSelect($columnOrIndexOrObject);
+        } elseif (is_object($columnOrIndexOrObject) && $tableOrAliasOrUniqueId !== null) {
+            // $column = new Column('(SELECT GROUP_CONCAT....)', 'someAlias')
+            $this->setUniqueId($tableOrAliasOrUniqueId);
+            $this->setSelect($columnOrIndexOrObject);
+        } else {
+            throw new \Exception('Column was not initiated correctly, please read the __construct docblock!');
+        }
+    }
+}
