453f3ab5c68c418a5f3701d94444401b3ff43708
Martin Keckeis <martin.keckeis1@gmail.com>
2013-05-02 18:30:14 +0200

    Move services from Module.php to config.module.php
    + Factories to invokables

--- config/module.config.php
+++ config/module.config.php
@@ -1,73 +1,7 @@
 <?php
+use Zend\ServiceManager\ServiceManager;
+
 return array(
-    'zfcDatagrid_dbAdapter' => array(
-        'driver' => 'Pdo_Sqlite',
-        'database' => 'data/ZfcDatagrid/testDb.sqlite'
-    ),
-    
-    'doctrine' => array(
-        'connection' => array(
-            'orm_zfcDatagrid' => array(
-                'driverClass' => 'Doctrine\DBAL\Driver\PDOSqlite\Driver',
-                'params' => array(
-                    'charset' => 'utf8',
-                    'path' => 'data/ZfcDatagrid/testDb.sqlite'
-                )
-            )
-        ),
-        
-        'configuration' => array(
-            'orm_zfcDatagrid' => array(
-                'metadata_cache' => 'array',
-                'query_cache' => 'array',
-                'result_cache' => 'array',
-                'driver' => 'orm_zfcDatagrid',
-                'generate_proxies' => true,
-                'proxy_dir' => 'data/ZfcDatagrid/Proxy',
-                'proxy_namespace' => 'ZfcDatagrid\Proxy',
-                'filters' => array()
-            )
-        ),
-        
-        'driver' => array(
-            'ZfcDatagrid_Driver' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(
-                    __DIR__ . '/../src/ZfcDatagrid/Examples/Entity'
-                )
-            ),
-            
-            'orm_zfcDatagrid' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\DriverChain',
-                'drivers' => array(
-                    'ZfcDatagrid\Examples\Entity' => 'ZfcDatagrid_Driver'
-                )
-            )
-        ),
-        
-        // now you define the entity manager configuration
-        'entitymanager' => array(
-            // This is the alternative config
-            'orm_zfcDatagrid' => array(
-                'connection' => 'orm_zfcDatagrid',
-                'configuration' => 'orm_zfcDatagrid'
-            )
-        ),
-        
-        'eventmanager' => array(
-            'orm_crawler' => array()
-        ),
-        
-        'sql_logger_collector' => array(
-            'orm_crawler' => array()
-        ),
-        
-        'entity_resolver' => array(
-            'orm_crawler' => array()
-        )
-    ),
-    
     'ZfcDatagrid' => array(
         
         'defaults' => array(
@@ -105,8 +39,65 @@ return array(
         )
     ),
     
+    'service_manager' => array(
+        'invokables' => array(
+            'zfcDatagrid.renderer.bootstrapTable' => 'ZfcDatagrid\Renderer\Html\BootstrapTable',
+            'zfcDatagrid.renderer.printPlain' => 'ZfcDatagrid\Renderer\Html\PrintPlain',
+            'zfcDatagrid.renderer.zendTable' => 'ZfcDatagrid\Renderer\Text\ZendTable',
+            
+            'zfcDatagrid.examples.data.phpArray' => 'ZfcDatagrid\Examples\Data\PhpArray',
+            'zfcDatagrid.examples.data.doctrine2' => 'ZfcDatagrid\Examples\Data\Doctrine2',
+            'zfcDatagrid.examples.data.zendSelect' => 'ZfcDatagrid\Examples\Data\ZendSelect',
+            '' => '',
+        ),
+        
+        'factories' => array(
+            'zfcDatagrid' => function  (ServiceManager $serviceManager)
+            {
+                $dataGrid = new \ZfcDatagrid\Datagrid();
+                $dataGrid->setOptions($serviceManager->get('config')['ZfcDatagrid']);
+                $dataGrid->setMvcEvent($serviceManager->get('application')
+                    ->getMvcEvent());
+                if ($serviceManager->has('translator') === true) {
+                    $dataGrid->setTranslator($serviceManager->get('translator'));
+                }
+                $dataGrid->init();
+        
+                return $dataGrid;
+            },
+            
+            'zfcDatagrid_dbAdapter' => function(ServiceManager $serviceManager){
+                return new \Zend\Db\Adapter\Adapter($serviceManager->get('config')['zfcDatagrid_dbAdapter']);
+            },
+        
+            // For the doctrine examples!
+            'doctrine.connection.orm_zfcDatagrid' => new \DoctrineORMModule\Service\DBALConnectionFactory('orm_zfcDatagrid'),
+            'doctrine.configuration.orm_zfcDatagrid' => new \DoctrineORMModule\Service\ConfigurationFactory('orm_zfcDatagrid'),
+            'doctrine.entitymanager.orm_zfcDatagrid' => new \DoctrineORMModule\Service\EntityManagerFactory('orm_zfcDatagrid'),
+        
+            'doctrine.driver.orm_zfcDatagrid' => new \DoctrineModule\Service\DriverFactory('orm_zfcDatagrid'),
+            'doctrine.eventmanager.orm_zfcDatagrid' => new \DoctrineModule\Service\EventManagerFactory('orm_zfcDatagrid'),
+            'doctrine.entity_resolver.orm_zfcDatagrid' => new \DoctrineORMModule\Service\EntityResolverFactory('orm_zfcDatagrid'),
+            'doctrine.sql_logger_collector.orm_zfcDatagrid' => new \DoctrineORMModule\Service\SQLLoggerCollectorFactory('orm_zfcDatagrid')
+        )
+    ),
+    
+
+    'view_manager' => array(
+    
+        'template_map' => array(
+            'zfc-datagrid/renderer/html/bootstrap-table' => __DIR__ . '/../view/zfc-datagrid/renderer/html/bootstrap-table.phtml',
+            'zfc-datagrid/renderer/html/print-plain' => __DIR__ . '/../view/zfc-datagrid/renderer/html/print-plain.phtml'
+        ),
+    
+        'template_path_stack' => array(
+            'ZfcDatagrid' => __DIR__ . '/../view'
+        )
+    ),
+    
+    
     /**
-     * Controller + routing for examples
+     * ONLY EXAMPLE CONFIGURATION BELOW!!!!!!
      */
     'controllers' => array(
         'invokables' => array(
@@ -177,15 +168,74 @@ return array(
         )
     ),
     
-    'view_manager' => array(
-        
-        'template_map' => array(
-            'zfc-datagrid/renderer/html/bootstrap-table' => __DIR__ . '/../view/zfc-datagrid/renderer/html/bootstrap-table.phtml',
-            'zfc-datagrid/renderer/html/print-plain' => __DIR__ . '/../view/zfc-datagrid/renderer/html/print-plain.phtml'
+    /**
+     * The ZF2 DbAdapter + Doctrine2 connection is must for examples!
+     */
+    'zfcDatagrid_dbAdapter' => array(
+        'driver' => 'Pdo_Sqlite',
+        'database' => 'data/ZfcDatagrid/testDb.sqlite'
+    ),
+    
+    'doctrine' => array(
+        'connection' => array(
+            'orm_zfcDatagrid' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOSqlite\Driver',
+                'params' => array(
+                    'charset' => 'utf8',
+                    'path' => 'data/ZfcDatagrid/testDb.sqlite'
+                )
+            )
         ),
         
-        'template_path_stack' => array(
-            'ZfcDatagrid' => __DIR__ . '/../view'
+        'configuration' => array(
+            'orm_zfcDatagrid' => array(
+                'metadata_cache' => 'array',
+                'query_cache' => 'array',
+                'result_cache' => 'array',
+                'driver' => 'orm_zfcDatagrid',
+                'generate_proxies' => true,
+                'proxy_dir' => 'data/ZfcDatagrid/Proxy',
+                'proxy_namespace' => 'ZfcDatagrid\Proxy',
+                'filters' => array()
+            )
+        ),
+        
+        'driver' => array(
+            'ZfcDatagrid_Driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__ . '/../src/ZfcDatagrid/Examples/Entity'
+                )
+            ),
+            
+            'orm_zfcDatagrid' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\DriverChain',
+                'drivers' => array(
+                    'ZfcDatagrid\Examples\Entity' => 'ZfcDatagrid_Driver'
+                )
+            )
+        ),
+        
+        // now you define the entity manager configuration
+        'entitymanager' => array(
+            // This is the alternative config
+            'orm_zfcDatagrid' => array(
+                'connection' => 'orm_zfcDatagrid',
+                'configuration' => 'orm_zfcDatagrid'
+            )
+        ),
+        
+        'eventmanager' => array(
+            'orm_crawler' => array()
+        ),
+        
+        'sql_logger_collector' => array(
+            'orm_crawler' => array()
+        ),
+        
+        'entity_resolver' => array(
+            'orm_crawler' => array()
         )
     )
 );
--- src/ZfcDatagrid/Module.php
+++ src/ZfcDatagrid/Module.php
@@ -3,12 +3,10 @@ namespace ZfcDatagrid;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
 use Zend\Console\Adapter\AdapterInterface as Console;
-use Zend\ServiceManager\ServiceManager;
-use ZfcDatagrid\Datagrid;
 
-class Module implements AutoloaderProviderInterface
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface, ConsoleUsageProviderInterface
 {
 
     public function getAutoloaderConfig ()
@@ -25,77 +23,6 @@ class Module implements AutoloaderProviderInterface
     public function getConfig ()
     {
         return include __DIR__ . '/../../config/module.config.php';
-    }
-
-    public function getServiceConfig ()
-    {
-        return array(
-            
-            'factories' => array(
-                'zfcDatagrid' => function  (ServiceManager $serviceManager)
-                {
-                    $dataGrid = new Datagrid();
-                    $dataGrid->setOptions($serviceManager->get('config')['ZfcDatagrid']);
-                    $dataGrid->setMvcEvent($serviceManager->get('application')
-                        ->getMvcEvent());
-                    if ($serviceManager->has('translator') === true) {
-                        $dataGrid->setTranslator($serviceManager->get('translator'));
-                    }
-                    $dataGrid->init();
-                    
-                    return $dataGrid;
-                },
-                
-                'zfcDatagrid.renderer.bootstrapTable' => function  (ServiceManager $serviceManager)
-                {
-                    return new Renderer\Html\BootstrapTable();
-                },
-                
-                'zfcDatagrid.renderer.printPlain' => function  (ServiceManager $serviceManager)
-                {
-                    return new Renderer\Html\PrintPlain();
-                },
-                
-                'zfcDatagrid.renderer.tcpdf' => function  (ServiceManager $serviceManager)
-                {
-                    return new Renderer\Export\Tcpdf();
-                },
-                
-                'zfcDatagrid.renderer.zendTable' => function  (ServiceManager $serviceManager)
-                {
-                    return new Renderer\Text\ZendTable();
-                },
-                
-                'zfcDatagrid.examples.data.phpArray' => function  (ServiceManager $serviceManager)
-                {
-                    return new Examples\Data\PhpArray();
-                },
-                
-                'zfcDatagrid.examples.data.doctrine2' => function  (ServiceManager $serviceManager)
-                {
-                    return new Examples\Data\Doctrine2();
-                },
-                
-                'zfcDatagrid.examples.data.zendSelect' => function  (ServiceManager $serviceManager)
-                {
-                    return new Examples\Data\ZendSelect();
-                },
-                
-                'zfcDatagrid_dbAdapter' => function(ServiceManager $serviceManager){
-                    return new \Zend\Db\Adapter\Adapter($serviceManager->get('config')['zfcDatagrid_dbAdapter']);
-                },
-                
-                // For the doctrine examples!
-                'doctrine.connection.orm_zfcDatagrid' => new \DoctrineORMModule\Service\DBALConnectionFactory('orm_zfcDatagrid'),
-                'doctrine.configuration.orm_zfcDatagrid' => new \DoctrineORMModule\Service\ConfigurationFactory('orm_zfcDatagrid'),
-                'doctrine.entitymanager.orm_zfcDatagrid' => new \DoctrineORMModule\Service\EntityManagerFactory('orm_zfcDatagrid'),
-                
-                'doctrine.driver.orm_zfcDatagrid' => new \DoctrineModule\Service\DriverFactory('orm_zfcDatagrid'),
-                'doctrine.eventmanager.orm_zfcDatagrid' => new \DoctrineModule\Service\EventManagerFactory('orm_zfcDatagrid'),
-                'doctrine.entity_resolver.orm_zfcDatagrid' => new \DoctrineORMModule\Service\EntityResolverFactory('orm_zfcDatagrid'),
-                'doctrine.sql_logger_collector.orm_zfcDatagrid' => new \DoctrineORMModule\Service\SQLLoggerCollectorFactory('orm_zfcDatagrid')
-            )
-        );
     }
 
     public function getConsoleUsage (Console $console)
