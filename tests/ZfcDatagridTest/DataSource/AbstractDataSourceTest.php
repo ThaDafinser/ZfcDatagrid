d8be5927246bea62a132010bca7d512c59fd620e
Martin Keckeis <martin.keckeis1@gmail.com>
2014-03-11 12:31:34 +0100

    Enable HHVM test again

--- tests/ZfcDatagridTest/DataSource/AbstractDataSourceTest.php
+++ tests/ZfcDatagridTest/DataSource/AbstractDataSourceTest.php
@@ -17,10 +17,6 @@ class AbstractDataSourceTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        if (defined('HPHP_VERSION') === true) {
-            $this->fail('HHVM Fatals');
-        }
-        
         $this->dsMock = $this->getMockForAbstractClass('ZfcDatagrid\DataSource\AbstractDataSource', array(
             array()
         ), '', false);
