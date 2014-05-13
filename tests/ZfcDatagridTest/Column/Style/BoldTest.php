1a201c4f5740d3bfd1955a8290b5b224030ca5cb
Martin Keckeis <martin.keckeis1@gmail.com>
2014-05-13 13:03:54 +0200

    php-cs-fixer

---
+++ tests/ZfcDatagridTest/Column/Style/BoldTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace ZfcDatagridTest\Column\Style;
+
+use PHPUnit_Framework_TestCase;
+use ZfcDatagrid\Column\Style;
+
+/**
+ * @group Column
+ * @covers ZfcDatagrid\Column\Style\Bold
+ */
+class BoldTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testCanCreateInstance()
+    {
+        $bold = new Style\Bold();
+    }
+}
