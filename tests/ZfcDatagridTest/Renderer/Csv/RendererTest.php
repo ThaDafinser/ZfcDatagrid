f46439f523c88e2ff5a0f2a29135b47a316f3ad4
Martin Keckeis <martin.keckeis1@gmail.com>
2014-05-13 12:31:48 +0200

    some tests

---
+++ tests/ZfcDatagridTest/Renderer/Csv/RendererTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace ZfcDatagridTest\Renderer\Csv;
+
+use PHPUnit_Framework_TestCase;
+use ZfcDatagrid\Renderer\Csv;
+
+/**
+ * @group Renderer
+ * @covers ZfcDatagrid\Renderer\Csv\Renderer
+ */
+class RendererTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testGetName()
+    {
+        $renderer = new Csv\Renderer();
+        
+        $this->assertEquals('csv', $renderer->getName());
+    }
+
+    public function testIsExport()
+    {
+        $renderer = new Csv\Renderer();
+        
+        $this->assertTrue($renderer->isExport());
+    }
+
+    public function testIsHtml()
+    {
+        $renderer = new Csv\Renderer();
+        
+        $this->assertFalse($renderer->isHtml());
+    }
+}
