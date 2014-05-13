08e7f0d457abd22b02e6610f1ae6eee86ba5e5e0
Martin Keckeis <martin.keckeis1@gmail.com>
2014-05-13 14:50:04 +0200

    Make config cacheable again

--- config/module.config.php
+++ config/module.config.php
@@ -61,23 +61,7 @@ return array(
                     'currentPage' => 'currentPage',
                     'sortColumns' => 'sortByColumns',
                     'sortDirections' => 'sortDirections'
-                ),
-                // Daterange bootstrapTable filter configuration example
-                'daterange' => array(
-                    'enabled' => false,
-                    'options' => array(
-                        'ranges' => array(
-                            'Today' => new \Zend\Json\Expr("[moment().startOf('day'), moment().endOf('day')]"),
-                            'Yesterday' => new \Zend\Json\Expr("[moment().subtract('days', 1), moment().subtract('days', 1)]"), 
-                            'Last 7 Days' => new \Zend\Json\Expr("[moment().subtract('days', 6), moment()]"),
-                            'Last 30 Days'=> new \Zend\Json\Expr("[moment().subtract('days', 29), moment()]"),
-                            'This Month' => new \Zend\Json\Expr("[moment().startOf('month'), moment().endOf('month')]"),
-                            'Last Month' => new \Zend\Json\Expr("[moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]"),
-                        ),
-                        'locale' => \Locale::getDefault(),
-                        'format' => 'DD/MM/YY HH:mm:ss',
-                    ),
-                ),
+                )
             ),
             
             'jqGrid' => array(
@@ -194,8 +178,8 @@ return array(
                 'delimiter' => ',',
                 'enclosure' => '"'
             )
-            
-        ),
+        )
+        ,
         
         // General parameters
         'generalParameterNames' => array(
--- src/ZfcDatagrid/Module.php
+++ src/ZfcDatagrid/Module.php
@@ -1,13 +1,9 @@
 <?php
 namespace ZfcDatagrid;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
-use Zend\ModuleManager\Feature\ServiceProviderInterface;
 use Zend\Console\Adapter\AdapterInterface as Console;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface, ConsoleUsageProviderInterface, ServiceProviderInterface
+class Module
 {
 
     public function getAutoloaderConfig()
@@ -16,7 +12,7 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface, Co
             'Zend\Loader\ClassMapAutoloader' => array(
                 __DIR__ . '/../../autoload_classmap.php'
             ),
-
+            
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
@@ -27,7 +23,34 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface, Co
 
     public function getConfig()
     {
-        return include __DIR__ . '/../../config/module.config.php';
+        $config = include __DIR__ . '/../../config/module.config.php';
+        $configNoCache = array(
+            'ZfcDatagrid' => array(
+                'renderer' => array(
+                    
+                    'bootstrapTable' => array(
+                        // Daterange bootstrapTable filter configuration example
+                        'daterange' => array(
+                            'enabled' => false,
+                            'options' => array(
+                                'ranges' => array(
+                                    'Today' => new \Zend\Json\Expr("[moment().startOf('day'), moment().endOf('day')]"),
+                                    'Yesterday' => new \Zend\Json\Expr("[moment().subtract('days', 1), moment().subtract('days', 1)]"),
+                                    'Last 7 Days' => new \Zend\Json\Expr("[moment().subtract('days', 6), moment()]"),
+                                    'Last 30 Days' => new \Zend\Json\Expr("[moment().subtract('days', 29), moment()]"),
+                                    'This Month' => new \Zend\Json\Expr("[moment().startOf('month'), moment().endOf('month')]"),
+                                    'Last Month' => new \Zend\Json\Expr("[moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]")
+                                ),
+                                'locale' => \Locale::getDefault(),
+                                'format' => 'DD/MM/YY HH:mm:ss'
+                            )
+                        )
+                    )
+                )
+            )
+        );
+        
+        return array_merge_recursive($config, $configNoCache);
     }
 
     public function getServiceConfig()
@@ -39,7 +62,7 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface, Co
                     'doctrine.connection.orm_zfcDatagrid' => new \DoctrineORMModule\Service\DBALConnectionFactory('orm_zfcDatagrid'),
                     'doctrine.configuration.orm_zfcDatagrid' => new \DoctrineORMModule\Service\ConfigurationFactory('orm_zfcDatagrid'),
                     'doctrine.entitymanager.orm_zfcDatagrid' => new \DoctrineORMModule\Service\EntityManagerFactory('orm_zfcDatagrid'),
-
+                    
                     'doctrine.driver.orm_zfcDatagrid' => new \DoctrineModule\Service\DriverFactory('orm_zfcDatagrid'),
                     'doctrine.eventmanager.orm_zfcDatagrid' => new \DoctrineModule\Service\EventManagerFactory('orm_zfcDatagrid'),
                     'doctrine.entity_resolver.orm_zfcDatagrid' => new \DoctrineORMModule\Service\EntityResolverFactory('orm_zfcDatagrid'),
@@ -47,7 +70,7 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface, Co
                 )
             );
         }
-
+        
         return array();
     }
 
@@ -57,7 +80,7 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface, Co
             'Display the example console datagrid',
             'datagrid person' => 'Show person datagrid',
             'datagrid category' => 'Show category datagrid',
-
+            
             'Options:',
             array(
                 '--page=NUMBER',
