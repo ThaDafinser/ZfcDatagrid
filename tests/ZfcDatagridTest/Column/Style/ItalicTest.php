dfd9a8ea5098d73330a239c354e74e5a83fa9912
Martin Keckeis <martin.keckeis1@gmail.com>
2014-05-13 13:11:18 +0200

    psr-2 travis

---
+++ tests/ZfcDatagridTest/Column/Style/ItalicTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace ZfcDatagridTest\Column\Style;
+
+use PHPUnit_Framework_TestCase;
+use ZfcDatagrid\Column\Style;
+
+/**
+ * @group Column
+ * @covers ZfcDatagrid\Column\Style\Italic
+ */
+class BoldTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testCanCreateInstance()
+    {
+        $bold = new Style\Italic();
+    }
+}
