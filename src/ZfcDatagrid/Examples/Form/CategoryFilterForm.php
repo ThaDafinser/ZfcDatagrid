105be4350f16cdacc926be0beac745224b4a713f
Martin Keckeis <martin.keckeis1@gmail.com>
2014-07-21 13:00:17 +0200

    Update CategoryFilterForm.php

--- src/ZfcDatagrid/Examples/Form/CategoryFilterForm.php
+++ src/ZfcDatagrid/Examples/Form/CategoryFilterForm.php
@@ -56,58 +56,6 @@ class CategoryFilterForm extends Form implements InputFilterAwareInterface
 
     public function getInputFilter()
     {
-        // if (!$this->inputFilter) {
-        // $inputFilter = new InputFilter();
-        // $factory = new InputFactory();
-
-        // $inputFilter->add($factory->createInput(array(
-        // 'name' => 'id',
-        // 'required' => true,
-        // 'filters' => array(
-        // array('name' => 'Int'),
-        // ),
-        // )));
-
-        // $inputFilter->add($factory->createInput(array(
-        // 'name' => 'artist',
-        // 'required' => true,
-        // 'filters' => array(
-        // array('name' => 'StripTags'),
-        // array('name' => 'StringTrim'),
-        // ),
-        // 'validators' => array(
-        // array(
-        // 'name' => 'StringLength',
-        // 'options' => array(
-        // 'encoding' => 'UTF-8',
-        // 'min' => 1,
-        // 'max' => 100,
-        // ),
-        // ),
-        // ),
-        // )));
-
-        // $inputFilter->add($factory->createInput(array(
-        // 'name' => 'title',
-        // 'required' => true,
-        // 'filters' => array(
-        // array('name' => 'StripTags'),
-        // array('name' => 'StringTrim'),
-        // ),
-        // 'validators' => array(
-        // array(
-        // 'name' => 'StringLength',
-        // 'options' => array(
-        // 'encoding' => 'UTF-8',
-        // 'min' => 1,
-        // 'max' => 100,
-        // ),
-        // ),
-        // ),
-        // )));
-
-        // $this->inputFilter = $inputFilter;
-        // }
         return array();
     }
 }
