46255ce8d092d617dcad09b3a632e8f5dbc07d3b
Martin Keckeis <martin.keckeis1@gmail.com>
2014-11-06 12:29:54 +0100

    bugfix

--- src/ZfcDatagrid/Renderer/JqGrid/Renderer.php
+++ src/ZfcDatagrid/Renderer/JqGrid/Renderer.php
@@ -203,7 +203,7 @@ class Renderer extends AbstractRenderer
                     }
 
                     $row[$column->getUniqueId()] = implode(' ', $actions);
-                } elseif ($column instanceof Column\Icon) {
+                } elseif ($column instanceof Column\Action\Icon) {
                     $row[$column->getUniqueId()] = $column->getIconClass();
                 }
             }
