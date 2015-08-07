289b2dd958c6563c0c4f786ca6bf0eb973ad67ea
ThaDafinser <martin.keckeis@thyssenkrupp.com>
2015-08-07 14:21:36 +0200

    remove ZF2 autoload + support only composer

--- Module.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Needed if not installed with composer
- */
-require_once __DIR__ . '/src/ZfcDatagrid/Module.php';
--- src/ZfcDatagrid/Module.php
+++ src/ZfcDatagrid/Module.php
@@ -3,20 +3,6 @@ namespace ZfcDatagrid;
 
 class Module
 {
-    public function getAutoloaderConfig()
-    {
-        return [
-            'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/../../autoload_classmap.php',
-            ],
-
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
-        ];
-    }
 
     public function getConfig()
     {
