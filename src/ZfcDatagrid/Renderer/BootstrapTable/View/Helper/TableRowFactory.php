41a26108db6e170b7a32b8425eef2ca0eac67c20
KoKsPfLaNzE <kokspflanze@gmx.de>
2016-04-05 23:20:14 +0200

    added getServiceLocator in both view-helper, we get a `Zend\ServiceManager\AbstractPluginManager` object so we have to use this method

--- src/ZfcDatagrid/Renderer/BootstrapTable/View/Helper/TableRowFactory.php
+++ src/ZfcDatagrid/Renderer/BootstrapTable/View/Helper/TableRowFactory.php
@@ -1,21 +1,22 @@
 <?php
 namespace ZfcDatagrid\Renderer\BootstrapTable\View\Helper;
 
+use Zend\ServiceManager\AbstractPluginManager;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class TableRowFactory implements FactoryInterface
 {
     /**
-     * @param  ServiceLocatorInterface $serviceLocator
+     * @param  ServiceLocatorInterface|AbstractPluginManager $serviceLocator
      * @return TableRow
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $tableRow = new TableRow();
-        if ($serviceLocator->has('translator')) {
+        if ($serviceLocator->getServiceLocator()->has('translator')) {
             /** @noinspection PhpParamsInspection */
-            $tableRow->setTranslator($serviceLocator->get('translator'));
+            $tableRow->setTranslator($serviceLocator->getServiceLocator()->get('translator'));
         }
 
         return $tableRow;
